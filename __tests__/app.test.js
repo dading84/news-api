788b3189a6d1646f9ec8ee4c700acbf7454f8e4a
Duncan <twoontheriver@gmail.com>
2022-02-14 14:10:10 +0000

    add test for status 200 article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
+const { convertTimestampToDate } = require("../db/data/helpers/utils");
 
 beforeEach(() => seed(data));
 afterAll(() => db.end());
@@ -30,6 +31,26 @@ describe("GET - /api/topics", () => {
       .expect(404)
       .then((res) => {
         expect(res.body.msg).toBe("Path not found!");
+      });
+  });
+});
+
+describe("GET - /api/articles/:article_id", () => {
+  test("status: 200 - should return an article matching the article_id in the endpoint", () => {
+    const expectedArticle = convertTimestampToDate({
+      article_id = 1,
+      title: "Living in the shadow of a great man",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "I find this existence challenging",
+      created_at: 1594329060000,
+      votes: 100
+    });
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.article).toEqual(expectedArticle);
       });
   });
 });
