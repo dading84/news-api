6d1c9f8f7108dd9bb5ea786a8466c48e4bcf8f44
Duncan <twoontheriver@gmail.com>
2022-02-14 12:27:00 +0000

    add test for 404

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,4 +24,7 @@ describe("GET - /api/topics", () => {
         });
       });
   });
+  test("status: 404 - should return with a status code of 404 if the path is not found", () => {
+    return request(app).get("/api/topicsss").expect(404);
+  });
 });
