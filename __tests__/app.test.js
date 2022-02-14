6df2b0bd8ff45925f0fa2b7f5c6dcf833660df8a
Duncan <twoontheriver@gmail.com>
2022-02-14 11:56:45 +0000

    add app with first test

---
+++ __tests__/app.test.js
@@ -0,0 +1,27 @@
+const app = require("../app.js");
+const request = require("supertest");
+const db = require("../db/connection.js");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+beforeEach(() => seed(data));
+afterAll(() => db.end());
+
+describe("GET - /api/topics", () => {
+  test("status: 200 - should return an array of 3 topic objects with slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.topics).toHaveLength(3);
+        res.body.topics.forEach((topic) => {
+          expect(topic).toEqual(
+            expect.objectContaining({
+              slug: expect.any(String),
+              description: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+});
