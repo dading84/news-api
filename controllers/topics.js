167e67d2b80e47325d2d00f015da4b56a67123ea
Duncan <twoontheriver@gmail.com>
2022-02-14 12:35:43 +0000

    removed comments

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,7 +1,6 @@
 const { selectTopics } = require("../models/topics");
 
 exports.getTopics = (req, res) => {
-  //console.log("inside controller");
   selectTopics().then((topics) => {
     res.status(200).send({ topics });
   });
--- models/topics.js
+++ models/topics.js
@@ -2,7 +2,6 @@ const db = require("../db/connection.js");
 
 exports.selectTopics = () => {
   return db.query("SELECT * FROM topics").then(({ rows }) => {
-    // console.log(rows, "<rows");
     return rows;
   });
 };
